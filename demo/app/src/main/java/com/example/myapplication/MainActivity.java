75aae43a6179f97318cf813cc5349a6bbf63e4e5
kaixuan.li <li.kaixuan@innodev.com.cn>
2019-03-13 18:13:46 +0800

    基于监听的事件处理机制，内部类方式最快

--- demo/app/src/main/java/com/example/myapplication/MainActivity.java
+++ demo/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,16 +2,58 @@ package com.example.myapplication;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.myapplication.R;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    private Button btnshow;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnshow = (Button) findViewById(R.id.linsten_button);
+        method_1(btnshow);
+        btnshow.setOnClickListener(new BtnClickListener());
+
+    }
+
+    //监听方式一：直接写匿名内部类
+    public void method_1(Button btnshow){
+        btnshow.setOnClickListener(new View.OnClickListener() {
+            //重写点击事件的处理方法onClick()
+            @Override
+            public void onClick(View v) {
+                //显示Toast信息
+                Toast.makeText(getApplicationContext(), "第一种监听方式", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    //监听方式二：定义一个内部类,实现View.OnClickListener接口,并重写onClick()方法
+    class BtnClickListener implements View.OnClickListener
+    {
+        @Override
+        public void onClick(View v) {
+            Toast.makeText(getApplicationContext(), "第二种监听方式", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    //监听方式三：用得较少要通过构造方法将组件传入使用;这样导致的结果就是代码不够简洁！
+    //监听方式四：直接使用Activity作为事件监听器，实现OnClickListener接口
+    @Override
+    public void onClick(View v) {
+        Toast.makeText(getApplicationContext(), "第四种监听方式", Toast.LENGTH_SHORT).show();
+    }
+
+    //监听方式五：直接绑定到标签，像js写法那样android:onClick="myclick"注意传一个组件参数
+    public void myclick(View source)
+    {
+        Toast.makeText(getApplicationContext(), "第五种监听方式", Toast.LENGTH_SHORT).show();
     }
 
     public void  radioButton(){
